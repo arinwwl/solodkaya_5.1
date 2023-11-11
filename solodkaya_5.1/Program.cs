55993210df9fb8c48e44134a71be4da75f60cde8
kbk.local <kbk.local@DESKTOP-INLDRME>
2023-11-11 16:08:17 +0200

    Добавьте файлы проекта.

---
+++ solodkaya_5.1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите массив символов из 16 элементов: ");
+string input = Console.ReadLine();
+char[] charArray = input.ToCharArray();
+int max = 0;
+int j = 0;
+foreach (char c in charArray)
+{
+
+    if (Char.IsDigit(c))
+    {
+        j = j * 10 + (c - '0');
+    }
+    else
+    {
+        if (j > max)
+        {
+            max = j;
+        }
+        j = 0;
+    }
+}
+
+if (j > max)
+{
+    max = j;
+}
+
+Console.WriteLine("Наибольшее целое число без учета знака: " + max);
